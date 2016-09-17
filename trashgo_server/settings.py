c5c1630318f8bf6adb52bf21cd9e6981db820e95
Xinyu Yang <colin.yangxinyu@outlook.com>
2016-09-17 07:04:25 -0700

    migrate to remote postgresql

--- trashgo_server/settings.py
+++ trashgo_server/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'trashgo_server.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'trashgo',
+        'USER': 'trashgo',
+        'PASSWORD': 'trashgogogo',
+        'HOST': '104.236.211.50',
+        'PORT': '',
     }
 }
 
