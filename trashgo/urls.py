5f7ae1ac97395a9fc49d833d25675e9a08c498d0
Xinyu Yang <colin.yangxinyu@outlook.com>
2016-09-17 11:16:49 -0700

    update url config

--- trashgo/urls.py
+++ trashgo/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 # from rest_framework import routers
-from rest_framework.urlpatterns import format_suffix_patterns
+# from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
@@ -10,13 +10,13 @@ from . import views
 urlpatterns = [
     url(r"^hotspots/$", views.HotspotView.as_view()),
     url((r"^hotspots/"
-         r"(?P<ne_lat>\d+\.?\d?)/"
-         r"(?P<ne_lng>\d+\.?\d?)/"
-         r"(?P<sw_lat>\d+\.?\d?)/"
-         r"(?P<sw_lng>\d+\.?\d?)/$"),
+         r"(?P<ne_lat>-?\d+\.?\d*)/"
+         r"(?P<ne_lng>-?\d+\.?\d*)/"
+         r"(?P<sw_lat>-?\d+\.?\d*)/"
+         r"(?P<sw_lng>-?\d+\.?\d*)/$"),
         views.HotspotWithinView.as_view()),
     url(r"^users/$", views.UserView.as_view()),
     url(r"^teams/$", views.TeamView.as_view()),
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = format_suffix_patterns(urlpatterns)
