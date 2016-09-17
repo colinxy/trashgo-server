489a7e66fbcbcaf7051ed851567f2883fda26a28
Xinyu Yang <colin.yangxinyu@outlook.com>
2016-09-17 01:37:49 -0700

    prepare GET view

--- trashgo/urls.py
+++ trashgo/urls.py
@@ -7,5 +7,7 @@ from . import views
 # router.register(r"hotspots", views.HotspotViewSet)
 
 urlpatterns = [
-    url(r"^hotspots", views.HotspotView.as_view()),
+    url(r"^hotspots/", views.HotspotView.as_view()),
+    url(r"^users/", views.UserView.as_view()),
+    url(r"^teams/", views.TeamView.as_view()),
 ]
--- trashgo/views.py
+++ trashgo/views.py
@@ -2,9 +2,28 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from .models import Hotspot
+from .models import Hotspot, User, Team
 
 
 class HotspotView(APIView):
     def get(self, request, formaat=None):
         return Response(Hotspot.objects.all())
+
+    def post(self, request, format=None):
+        pass
+
+
+class UserView(APIView):
+    def get(self, request, format=None):
+        return Response(User.objects.all())
+
+    def post(self, request, format=None):
+        pass
+
+
+class TeamView(APIView):
+    def get(self, request, format=None):
+        return Response(Team.objects.all())
+
+    def post(self, request, format=None):
+        pass
